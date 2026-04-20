4617c871bc6bd87f483a0284f74cdfb34beed437
idasheks-lang <idasheks@gmail.com>
2026-04-20 12:12:23 +0300

    loops

---
+++ practice/test1.py
@@ -0,0 +1,31 @@
+print("Hello python")
+
+name = "John"
+name2= "John"
+num = 25
+balance = 159.258
+flag = True
+
+# print(type(name))
+# print(type(num))
+"""print(type(balance))
+print(type(flag))"""
+
+print("Using Range")
+
+for i in range(2, 18, 2):
+    print(i)
+# never reaches max value(max-1)
+
+for i in range(18):
+    if i ==17:
+        print(i)     
+  
+# print(id(name)) 
+# print(id(name2)) 
+
+# items = [1,2,3,4,5,6,7,8]
+# for item in items:
+#   print(item)
+  
+  
